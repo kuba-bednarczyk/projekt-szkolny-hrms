c6199f65da114381cc93e75992667452a7932fa0
Kuba <kubabednarczyk2003@gmail.com>
2022-01-27 17:39:07 +0100

    make main-page and organize project structure

--- php/logowanie.php
+++ php/logowanie.php
@@ -9,10 +9,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logowanie</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/logowanie.css">
+    <link rel="stylesheet" href="../css/logowanie.css">
 </head>
 <body>
 
+<a href="../index.html" class="go-back">
+    <i class="fas fa-arrow-left"></i><br>
+    <span>strona główna</span>
+</a>
+
+<div class="container">
     <div class="login">
         <div class="login__logo">
             <i class="fas fa-users"></i>
@@ -51,6 +57,8 @@
             </form>
         </div>
     </div>
+</div>
+    
 
 </body>
 </html>
